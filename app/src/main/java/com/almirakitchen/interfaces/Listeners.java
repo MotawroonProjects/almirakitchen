ce1f5853c3413391d512ac555f7047c87e3fd6b6
HP <info@linkcom.tech>
2022-02-16 14:33:05 +0200

    Initial commit

---
+++ app/src/main/java/com/almirakitchen/interfaces/Listeners.java
@@ -0,0 +1,15 @@
+package com.almirakitchen.interfaces;
+
+
+public interface Listeners {
+
+
+
+
+    interface BackListener
+    {
+        void back();
+    }
+
+
+}
---
+++ app/src/main/java/com/almirakitchen/models/StatusResponse.java
@@ -0,0 +1,12 @@
+package com.almirakitchen.models;
+
+import java.io.Serializable;
+
+public class StatusResponse implements Serializable {
+    protected int status;
+
+    public int getStatus() {
+        return status;
+    }
+
+}
